441338a50daf213cee0f1c83a74efd16b014b8fe
Peter Harris <peter@harr.ca>
2021-01-17 16:14:10 -0500

    Shade walls based on angle from the light source

--- draw.go
+++ draw.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image"
+	"math"
 	"sync"
 )
 
@@ -31,6 +32,11 @@ func (m *MapData) defaults() {
 	m.LightAngle = 10
 }
 
+type fromType struct {
+	dist  uint8
+	angle int16
+}
+
 func (m *MapData) draw() {
 	width := 0
 	height := len(m.line) * m.TileSize
@@ -39,8 +45,10 @@ func (m *MapData) draw() {
 	}
 
 	floor := make([][]bool, height)
+	angle := make([][]int16, height)
 	for i := range floor {
 		floor[i] = make([]bool, width)
+		angle[i] = make([]int16, width)
 	}
 
 	for y, row := range m.line {
@@ -53,17 +61,61 @@ func (m *MapData) draw() {
 						floor[y*m.TileSize+dy][x*m.TileSize+dx] = true
 					}
 				}
+				// angles
+				leftempty := m.line[y][x-1] == ' '
+				rightempty := m.line[y][x+1] == ' '
+				aboveempty := m.line[y-1][x] == ' '
+				belowempty := m.line[y+1][x] == ' '
+				if leftempty {
+					for dy := 0; dy < m.TileSize; dy++ {
+						angle[y*m.TileSize+dy][x*m.TileSize] = 90
+						angle[y*m.TileSize+dy][x*m.TileSize-1] = 90
+					}
+				}
+				if rightempty {
+					for dy := 0; dy < m.TileSize; dy++ {
+						angle[y*m.TileSize+dy][(x+1)*m.TileSize-1] = 270
+						angle[y*m.TileSize+dy][(x+1)*m.TileSize] = 270
+					}
+				}
+				if aboveempty {
+					for dx := 0; dx < m.TileSize; dx++ {
+						angle[y*m.TileSize][x*m.TileSize+dx] = 180
+						angle[y*m.TileSize-1][x*m.TileSize+dx] = 180
+					}
+					if !rightempty {
+						angle[y*m.TileSize][(x+1)*m.TileSize-1] = 180
+					}
+					if !leftempty {
+						angle[y*m.TileSize][x*m.TileSize] = 180
+					}
+				}
+				if belowempty {
+					for dx := 0; dx < m.TileSize; dx++ {
+						angle[(y+1)*m.TileSize-1][x*m.TileSize+dx] = 360
+						angle[(y+1)*m.TileSize][x*m.TileSize+dx] = 360
+					}
+					if !rightempty {
+						angle[(y+1)*m.TileSize-1][(x+1)*m.TileSize-1] = 360
+					}
+					if !leftempty {
+						angle[(y+1)*m.TileSize-1][x*m.TileSize] = 360
+					}
+				}
 			case '\\':
 				// lower-left is solid
 				before := true
 				after := false
+				a := int16(180 + 45)
 				if m.line[y][x+1] == '#' || m.line[y-1][x] == '#' {
 					// strong rule: upper-right is solid
 					before, after = after, before
+					a -= 180
 				} else if (m.line[y][x+1] != ' ' || m.line[y-1][x] != ' ') &&
 					m.line[y][x-1] != '#' && m.line[y+1][x] != '#' {
 					// weak rule: upper-right is solid if we see upper-right and don't see solid below/left
 					before, after = after, before
+					a -= 180
 				}
 				for dy := 0; dy < m.TileSize; dy++ {
 					for dx := 0; dx < m.TileSize; dx++ {
@@ -72,19 +124,23 @@ func (m *MapData) draw() {
 							val = before
 						}
 						floor[y*m.TileSize+dy][x*m.TileSize+dx] = val
+						angle[y*m.TileSize+dy][x*m.TileSize+dx] = a
 					}
 				}
 			case '/':
 				// lower-right is solid
 				before := true
 				after := false
+				a := int16(180 - 45)
 				if m.line[y][x-1] == '#' || m.line[y-1][x] == '#' {
 					// strong: upper-left is solid
 					before, after = after, before
+					a += 180
 				} else if (m.line[y][x-1] != ' ' || m.line[y-1][x] != ' ') &&
 					m.line[y][x+1] != '#' && m.line[y+1][x] != '#' {
 					// weak: upper-left is solid
 					before, after = after, before
+					a += 180
 				}
 				for dy := 0; dy < m.TileSize; dy++ {
 					for dx := 0; dx < m.TileSize; dx++ {
@@ -93,14 +149,18 @@ func (m *MapData) draw() {
 							val = before
 						}
 						floor[y*m.TileSize+dy][x*m.TileSize+dx] = val
+						angle[y*m.TileSize+dy][x*m.TileSize+dx] = a
 					}
 				}
 			case '>':
 				before := true
 				after := false
+				firsta := int16(360 - 45)
+				seconda := int16(180 + 45)
 				if m.line[y][x+1] == '#' {
 					// right is solid
 					before, after = after, before
+					firsta, seconda = seconda+180, firsta+180
 				}
 				for dy := 0; dy < m.TileSize; dy++ {
 					for dx := 0; dx < m.TileSize; dx++ {
@@ -110,14 +170,22 @@ func (m *MapData) draw() {
 							val = before
 						}
 						floor[y*m.TileSize+dy][x*m.TileSize+dx] = val
+						seta := firsta
+						if dy*2 > m.TileSize {
+							seta = seconda
+						}
+						angle[y*m.TileSize+dy][x*m.TileSize+dx] = seta
 					}
 				}
 			case '<':
 				before := true
 				after := false
+				firsta := int16(45)
+				seconda := int16(180 - 45)
 				if m.line[y][x-1] == '#' {
-					// right is solid
+					// left is solid
 					before, after = after, before
+					firsta, seconda = seconda+180, firsta+180
 				}
 				for dy := 0; dy < m.TileSize; dy++ {
 					for dx := 0; dx < m.TileSize; dx++ {
@@ -127,14 +195,22 @@ func (m *MapData) draw() {
 							val = before
 						}
 						floor[y*m.TileSize+dy][x*m.TileSize+dx] = val
+						seta := firsta
+						if dy*2 > m.TileSize {
+							seta = seconda
+						}
+						angle[y*m.TileSize+dy][x*m.TileSize+dx] = seta
 					}
 				}
 			case 'v':
 				before := true
 				after := false
+				firsta := int16(45)
+				seconda := int16(360 - 45)
 				if m.line[y+1][x] == '#' {
-					// right is solid
+					// below is solid
 					before, after = after, before
+					firsta, seconda = seconda+180, firsta+180
 				}
 				for dy := 0; dy < m.TileSize; dy++ {
 					for dx := 0; dx < m.TileSize; dx++ {
@@ -144,14 +220,22 @@ func (m *MapData) draw() {
 							val = before
 						}
 						floor[y*m.TileSize+dy][x*m.TileSize+dx] = val
+						seta := firsta
+						if dx*2 > m.TileSize {
+							seta = seconda
+						}
+						angle[y*m.TileSize+dy][x*m.TileSize+dx] = seta
 					}
 				}
 			case '^':
 				before := true
 				after := false
+				firsta := int16(180 + 45)
+				seconda := int16(180 - 45)
 				if m.line[y-1][x] == '#' {
 					// above is solid
 					before, after = after, before
+					firsta, seconda = seconda+180, firsta+180
 				}
 				for dy := 0; dy < m.TileSize; dy++ {
 					for dx := 0; dx < m.TileSize; dx++ {
@@ -161,6 +245,11 @@ func (m *MapData) draw() {
 							val = before
 						}
 						floor[y*m.TileSize+dy][x*m.TileSize+dx] = val
+						seta := firsta
+						if dx*2 > m.TileSize {
+							seta = seconda
+						}
+						angle[y*m.TileSize+dy][x*m.TileSize+dx] = seta
 					}
 				}
 			default:
@@ -170,21 +259,43 @@ func (m *MapData) draw() {
 			}
 		}
 	}
+	// fill in corners
+	for y := 1; y < len(m.line)-1; y++ {
+		for x := 1; x < len(m.line[0]); x++ {
+			count := 0
+			total := 0
+			for circle := 0; circle < 4; circle++ {
+				if a := angle[y*m.TileSize-(circle/2)][x*m.TileSize-(circle&1)]; a != 0 {
+					count++
+					total += int(a % 360)
+				}
+			}
+			if count > 0 {
+				for circle := 0; circle < 4; circle++ {
+					if angle[y*m.TileSize-(circle/2)][x*m.TileSize-(circle&1)] == 0 {
+						angle[y*m.TileSize-(circle/2)][x*m.TileSize-(circle&1)] = int16(total / count)
+					}
+				}
+			}
+		}
+	}
 
 	// initialize
-	fromwall := make([][]uint8, 0, len(floor))
-	fromfloor := make([][]uint8, 0, len(floor))
-	for _, row := range floor {
-		wallrow := make([]uint8, 0, len(row))
-		floorrow := make([]uint8, 0, len(row))
-		for _, f := range row {
+	fromwall := make([][]fromType, 0, len(floor))
+	fromfloor := make([][]fromType, 0, len(floor))
+	for y, row := range floor {
+		wallrow := make([]fromType, 0, len(row))
+		floorrow := make([]fromType, 0, len(row))
+		for x, f := range row {
+			a := angle[y][x]
+			wall := fromType{dist: 0, angle: a}
+			floo := fromType{dist: 255, angle: a}
 			if f {
-				wallrow = append(wallrow, 255)
-				floorrow = append(floorrow, 0)
-			} else {
-				wallrow = append(wallrow, 0)
-				floorrow = append(floorrow, 255)
+				wall.dist = 255
+				floo.dist = 0
 			}
+			wallrow = append(wallrow, wall)
+			floorrow = append(floorrow, floo)
 		}
 		fromwall = append(fromwall, wallrow)
 		fromfloor = append(fromfloor, floorrow)
@@ -192,10 +303,10 @@ func (m *MapData) draw() {
 	// flood
 	var floodwait sync.WaitGroup
 	floodwait.Add(2)
-	f := func(what [][]uint8) {
+	f := func(what [][]fromType) {
 		for y := 3; y < len(floor)-3; y++ {
 			for x := 3; x < len(floor[y])-3; x++ {
-				flood(x, y, what)
+				flood(x, y, what, angle[y][x])
 			}
 		}
 		floodwait.Done()
@@ -205,6 +316,7 @@ func (m *MapData) draw() {
 	floodwait.Wait()
 
 	const opaque = uint8(255)
+	const toRad = math.Pi / 180
 	i := image.NewNRGBA(image.Rect(0, 0, width, height))
 	m.pic = i
 	offset := 0
@@ -213,35 +325,41 @@ func (m *MapData) draw() {
 			if f {
 				val := m.Shadow
 				fw := fromwall[y][x]
-				if fw > 0 {
-					fw--
+				if fw.dist > 0 {
+					fw.dist--
 				}
-				if int(fw) < m.WallSize/2 {
-					scale := float64(m.WallSize)/2 - float64(fw)
-					scale /= float64(m.WallSize)
+				if int(fw.dist) < m.WallSize/2 {
+					wallAngle := float64((m.LightAngle+360-fw.angle)%360) * toRad
+					wallBoost := uint8(float64(m.Light) * math.Cos(wallAngle))
+					scale := float64(m.WallSize)/2 - float64(fw.dist)
+					scale /= float64(m.WallSize + 1)
 					wall := m.WallBottom
 					wall += uint8(float64(m.WallTop-m.WallBottom) * scale)
+					wall += wallBoost
 					i.Pix[offset+0] = wall
 					i.Pix[offset+1] = wall
 					i.Pix[offset+2] = wall
 					i.Pix[offset+3] = opaque
 				} else {
-					fw -= uint8(m.WallSize / 2)
-					dist := float64(fw)/(float64(m.TileSize)*m.ShadowWidth) + 1
+					fw.dist -= uint8(m.WallSize / 2)
+					dist := float64(fw.dist)/(float64(m.TileSize)*m.ShadowWidth) + 1
 					extra := uint8(float64(m.ShadowDepth) / dist)
 					val += extra
 					i.Pix[offset+3] = val
 				}
 			} else {
 				ff := fromfloor[y][x]
-				if ff > 0 {
-					ff--
+				if ff.dist > 0 {
+					ff.dist--
 				}
-				if int(ff) < (m.WallSize+1)/2 {
-					scale := float64(m.WallSize+1)/2 - float64(ff)
-					scale /= float64(m.WallSize)
+				if int(ff.dist) < (m.WallSize+1)/2 {
+					wallAngle := float64((m.LightAngle+360-ff.angle)%360) * toRad
+					wallBoost := uint8(float64(m.Light) * math.Cos(wallAngle))
+					scale := float64(m.WallSize+1)/2 - float64(ff.dist)
+					scale /= float64(m.WallSize + 1)
 					wall := m.WallTop
 					wall -= uint8(float64(m.WallTop-m.WallBottom) * scale)
+					wall += wallBoost
 					i.Pix[offset+0] = wall
 					i.Pix[offset+1] = wall
 					i.Pix[offset+2] = wall
@@ -253,64 +371,72 @@ func (m *MapData) draw() {
 	}
 }
 
-func flood(x int, y int, chart [][]uint8) {
+func flood(x int, y int, chart [][]fromType, setAngle int16) {
 	this := chart[y][x]
-	if this >= 254 {
+	if this.dist >= 254 {
 		return
 	}
-	this++
-	if chart[y][x-1] > this {
+	this.dist++
+	if chart[y][x-1].dist > this.dist {
+		this.angle = setAngle
 		chart[y][x-1] = this
 		if x-3 > 0 {
-			flood(x-1, y, chart)
+			flood(x-1, y, chart, setAngle)
 		}
 	}
-	if chart[y-1][x] > this {
+	if chart[y-1][x].dist > this.dist {
+		this.angle = setAngle
 		chart[y-1][x] = this
 		if y-3 > 0 {
-			flood(x, y-1, chart)
+			flood(x, y-1, chart, setAngle)
 		}
 	}
-	if chart[y+1][x] > this {
+	if chart[y+1][x].dist > this.dist {
+		this.angle = setAngle
 		chart[y+1][x] = this
 		if y+3 < len(chart)-1 {
-			flood(x, y+1, chart)
+			flood(x, y+1, chart, setAngle)
 		}
 	}
-	if chart[y][x+1] > this {
+	if chart[y][x+1].dist > this.dist {
+		this.angle = setAngle
 		chart[y][x+1] = this
 		if x+3 < len(chart[y])-1 {
-			flood(x+1, y, chart)
+			flood(x+1, y, chart, setAngle)
 		}
 	}
 
 	// Soften diagonals somewhat
-	if this >= 253 {
+	if true || this.dist >= 253 {
 		return
 	}
-	this += 2
-	if chart[y-2][x-2] > this {
+	this.dist += 2
+	if chart[y-2][x-2].dist > this.dist {
+		this.angle = setAngle
 		chart[y-2][x-2] = this
 		if x-3 > 0 && y-3 > 0 {
-			flood(x-2, y-2, chart)
+			flood(x-2, y-2, chart, setAngle)
 		}
 	}
-	if chart[y-2][x+2] > this {
+	if chart[y-2][x+2].dist > this.dist {
+		this.angle = setAngle
 		chart[y-2][x+2] = this
 		if x+3 < len(chart[y])-1 && y-3 > 0 {
-			flood(x+2, y-2, chart)
+			flood(x+2, y-2, chart, setAngle)
 		}
 	}
-	if chart[y+2][x+2] > this {
+	if chart[y+2][x+2].dist > this.dist {
+		this.angle = setAngle
 		chart[y+2][x+2] = this
 		if x+3 < len(chart[y])-1 && y+3 < len(chart)-1 {
-			flood(x+2, y+2, chart)
+			flood(x+2, y+2, chart, setAngle)
 		}
 	}
-	if chart[y+2][x-2] > this {
+	if chart[y+2][x-2].dist > this.dist {
+		this.angle = setAngle
 		chart[y+2][x-2] = this
 		if x-3 > 0 && y+3 < len(chart)-1 {
-			flood(x-2, y+2, chart)
+			flood(x-2, y+2, chart, setAngle)
 		}
 	}
 }
